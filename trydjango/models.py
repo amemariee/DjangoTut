c0f6b3244deb27e63ec680d82cdfd60ae668615b
Amy Wall <awall@csumb.edu>
2021-04-22 05:00:52 +0000

    Added Models.py

---
+++ trydjango/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    summary = models.TextField()
